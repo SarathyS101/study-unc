539a26e1d033a526a5fa72d9a617a4d423f3ca35
SarathyS101 <se.sarathy17@gmail.com>
2025-06-07 20:24:09 -0400

    help me

--- scraper/scrape_upload.py
+++ scraper/scrape_upload.py
@@ -184,6 +184,19 @@ SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 assert SUPABASE_KEY and SUPABASE_KEY.startswith("eyJ"), "Service role key not loaded!"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+def run_sql_file(path: str):
+    """
+    Splits SQL file into statements and runs each via run_sql RPC.
+    """
+    with open(path, 'r') as f:
+        raw_sql = f.read()
+    for statement in sqlparse.split(raw_sql):
+        stmt = statement.strip()
+        if not stmt:
+            continue
+        supabase.rpc('run_sql', {'sql': stmt}).execute()
+
+
 # ====== 2) Scraper Function ======
 def scrape_subject(term: str, subject_code: str):
     """
@@ -307,6 +320,7 @@ if __name__ == "__main__":
     subject_codes = [
        "APPL", "AAAD", "ACSM", "ADJU", "AERO", "AFAM", "AFRI", "AHEC", "AHSC", "AIRS", "AMST", "ANA", "ANAT", "ANES", "ANGL", "ANS", "ANSC", "ANTH", "APSM", "ARA", "ARAB", "ARCH", "ARGE", "ARMY", "AROT", "ART", "ARTH", "ARTS", "ASCI", "ASCM", "ASHV", "ASIA", "ASTR", "BACT", "BAE", "BBSP", "BCB", "BCH", "BCHM", "BCS", "BEIJ", "BENG", "BERL", "BIOC", "BIOL", "BIOM", "BIOS", "BIOX", "BMA", "BME", "BMME", "BOE", "BOIC", "BOLO", "BOT", "BOTN", "BRIS", "BSCI", "BUIS", "BULG", "BUSA", "BUSG", "BUSI", "BUSS", "CAPS", "CATA", "CBAM", "CBIO", "CBMC", "CBPH", "CDFS", "CE", "CELT", "CENG", "CEOM", "CERT", "CGL", "CHE", "CHEM", "CHER", "CHIN", "CHIP", "CHPM", "CHSC", "CITZ", "CLAR", "CLAS", "CLIC", "CLIT", "CLSC", "CLSK", "CLST", "CMPL", "COML", "COMM", "COMP", "COPE", "CORE", "COSI", "CPXM", "CRMH", "CS", "CYTO", "CZCH", "DATA", "DATE", "DDDD", "DECO", "DENG", "DENT", "DERM", "DESN", "DHED", "DHYG", "DNDG", "DNEC", "DNED", "DPET", "DPMP", "DPOP", "DPPE", "DRAM", "DTCH", "EAST", "EC", "ECOL", "ECON", "ED", "ED1C", "EDCI", "EDFO", "EDIN", "EDMX", "EDSP", "EDUC", "EDUX", "EE", "EENG", "EGR", "ELAW", "EMES", "ENDO", "ENEC", "ENGL", "ENGM", "ENGR", "ENST", "ENT", "ENUR", "ENVR", "EPID", "ERMD", "EURO", "EXSS", "EXTN", "FARE", "FES", "FMED", "FMME", "FOLK", "FOR", "FORE", "FORS", "FRED", "FREN", "FSCI", "GEN", "GEOG", "GEOL", "GERJ", "GERM", "GHAN", "GLBE", "GLBL", "GN", "GNE", "GNET", "GOTT", "GOVT", "GRAD", "GREK", "GSA", "GSLL", "HAD", "HADA", "HADM", "HAUS", "HBEH", "HBHE", "HCTS", "HDL", "HE", "HEBR", "HECO", "HEED", "HIND", "HIST", "HLTH", "HMSC", "HMST", "HMTS", "HNRS", "HNUR", "HOME", "HORT", "HPAA", "HPM", "HSCI", "HST", "HUNG", "HUSA", "HYGI", "IBMS", "ICMU", "ICRS", "ICSR", "IDST", "IENG", "IEP", "IHMS", "IIOC", "IMMU", "INDC", "INDO", "INDR", "INFO", "INLS", "INTI", "INTS", "ISO", "ISRA", "ITAL", "JAP", "JAPN", "JOMC", "JOUR", "JWST", "KANS", "KFM", "KOR", "LAQ", "LAR", "LARS", "LATN", "LAW", "LEED", "LFIT", "LGLA", "LIBS", "LIMA", "LING", "LOND", "LSA", "LSEC", "LSRA", "LSSM", "LTAM", "LVE", "LW", "LYON", "MA", "MAC", "MACD", "MACF", "MAE", "MAHP", "MANC", "MANS", "MASC", "MAT", "MATE", "MATH", "MAYA", "MBA", "MBIO", "MCHL", "MCRO", "MDPH", "MDSP", "MEDC", "MEDF", "MEDI", "MEDT", "MEEN", "MEJO", "MENG", "MENH", "MESE", "METR", "MEXI", "MHCH", "MIC", "MICR", "MILS", "MISC", "MNDG", "MNGT", "MODC", "MONT", "MOPH", "MOPL", "MPED", "MS", "MSBS", "MSCI", "MSMS", "MTEC", "MTSC", "MUSC", "MXCL", "MYCO", "NANZ", "NAVS", "NBIO", "NDSS", "NE", "NENG", "NEUR", "NEUS", "NORW", "NSCI", "NSP", "NT", "NURS", "NUSJ", "NUTR", "OBGN", "OBIO", "OCBM", "OCCT", "OCEN", "OCSC", "ODTP", "OMED", "OMSU", "OPER", "OPHT", "OR", "ORAD", "ORDI", "ORLN", "ORPA", "ORSA", "ORSU", "ORTH", "ORTS", "OTOL", "P-LI", "PACE", "PADM", "PADS", "PALP", "PARA", "PASC", "PATH", "PATY", "PEDI", "PEDO", "PEDS", "PERI", "PERS", "PERU", "PEW", "PHAD", "PHAR", "PHCG", "PHCH", "PHCO", "PHCY", "PHED", "PHIL", "PHPR", "PHRS", "PHS", "PHTH", "PHYA", "PHYE", "PHYI", "PHYS", "PHYT", "PHYY", "PLAN", "PLCY", "PLNT", "PLSH", "PLTM", "PMED", "PO", "POLI", "POLT", "PORT", "PP", "PPES", "PPOL", "PPS", "PREV", "PROD", "PROS", "PRSN", "PS", "PSNU", "PSY", "PSYC", "PSYI", "PSYS", "PSYY", "PUBA", "PUBH", "PUBP", "PUPA", "PVME", "PWAD", "PYSI", "QHCH", "RADG", "RADI", "RADY", "RECR", "REL", "RELI", "REST", "RFIX", "RHAB", "RLGE", "ROMA", "ROML", "RPSY", "RTVM", "RUES", "RUMA", "RUSS", "SADM", "SANS", "SCLL", "SECR", "SERB", "SEVI", "SIEN", "SLAV", "SNVR", "SOC", "SOCI", "SOCM", "SOIL", "SOMP", "SOWO", "SPAN", "SPCH", "SPCY", "SPHG", "SPHS", "SSAP", "SSC", "SSCI", "ST", "STA", "STAN", "STAT", "STOR", "SUOP", "SURG", "SURS", "SURY", "SUSS", "SWAH", "SWED", "TAML", "TEXT", "THER", "TOXC", "TOXI", "TREQ", "TRXN", "TUBI", "TURK", "UBDS", "UKRN", "UNI", "UNIV", "URES", "VET", "VIET", "WGST", "WMST", "WOLL", "WOLO", "YIDI", "YORU", "ZOOL"
     ]
+    run_sql_file('clear_appended_tables.sql')
     for subj in subject_codes:
         print(f"Scraping {subj} for {term}…")
         try:
@@ -315,5 +329,5 @@ if __name__ == "__main__":
             upload_to_supabase(result_rows)
         except Exception as exc:
             print(f"  ✖ Failed on {subj}: {exc}")
-
+    run_sql_file('update_free_slots.sql')
     print("✅ All subjects complete.")
